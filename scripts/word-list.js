fa0ce8a7174c7f4edbc68c3b46b14ef7397458a0
Maria Allyza Toquire <mmatoquire@tip.edu.ph>
2023-11-10 23:41:47 +0800

    commit

---
+++ scripts/word-list.js
@@ -0,0 +1,34 @@
+const wordList = [
+    {
+        word: "notion",
+        hint: "a digital workspace that adapts to your needs in productivity"
+    },
+    {
+        word: "microsoftstudentcommunity",
+        hint: "a recognized student organization and a tech community in T.I.P. Manila driven by a passion for different technologies, especially Microsoft technologies."
+    },
+    {
+        word: "electro",
+        hint: "one of the seven Elements. Its associated Archon is Beelzebul, whose domain is Inazuma."
+    },
+    {
+        word: "anemo",
+        hint: "associated Archon is Barbatos, whose domain is Mondstadt"
+    },
+    {
+        word: "monstadst",
+        hint: "A Genshin Impact region known for its vast landscapes and diverse elements."
+    },
+    {
+        word: "venti",
+        hint: " A powerful and enigmatic character who wields Anemo and is associated with Mondstadt."
+    },
+    {
+        word: "sucrose",
+        hint: "An Anemo character with a mischievous nature and a penchant for wind-based pranks"
+    },
+    {
+        word: "beidou",
+        hint: " An Electro character and a key figure in the Crux Fleet, known for her daring and adventurous spirit."
+    },
+]
